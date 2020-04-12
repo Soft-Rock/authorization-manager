a4e1ae9ebf776cbbeafa878463e24e59d34a9d5f
David Gracia Celemendi <david.gracia.celemendi@gmail.com>
2020-04-12 20:04:37 +0200

    change

--- src/main/kotlin/authorization/manager/Permission.kt
+++ src/main/kotlin/authorization/manager/Permission.kt
@@ -2,4 +2,8 @@ package authorization.manager
 
 import java.util.*
 
-data class Permission(val identifier: UUID, val description: String)
+data class Permission(val identifier: UUID, val description: String) {
+    fun setDescription(description: String): Permission {
+        return copy(description = description)
+    }
+}
--- src/main/kotlin/authorization/manager/Role.kt
+++ src/main/kotlin/authorization/manager/Role.kt
@@ -1,3 +1,25 @@
 package authorization.manager
 
-data class Role(val permissions: Set<Permission>)
+import java.util.*
+
+data class Role(val identifier: UUID, val permissions: Set<Permission>) {
+    fun assignPermission(permission: Permission): Role {
+        return copy(permissions = this.permissions.plusElement(permission))
+    }
+
+    fun assignPermissions(permissions: Set<Permission>): Role {
+        return copy(permissions = this.permissions.plus(permissions))
+    }
+
+    fun withdrawPermission(permission: Permission): Role {
+        return copy(permissions = this.permissions.minus(permission))
+    }
+
+    fun withdrawPermissions(permissions: Set<Permission>): Role {
+        return copy(permissions = this.permissions.minus(permissions))
+    }
+
+    fun hasAssigned(permission: Permission): Boolean {
+        return permissions.contains(permission)
+    }
+}
