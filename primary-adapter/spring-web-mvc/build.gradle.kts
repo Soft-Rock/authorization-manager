2ca002d7dd5ce08e80643a53d20bc1cde25efaad
David Gracia Celemendi <david.gracia.celemendi@gmail.com>
2020-04-13 21:24:50 +0200

    change

--- primary-adapter/spring-web-mvc/build.gradle.kts
+++ primary-adapter/spring-web-mvc/build.gradle.kts
@@ -3,6 +3,11 @@ plugins {
     `java-library`
     id("org.springframework.boot") version "2.2.6.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
+    kotlin("plugin.spring") version "1.3.71"
+}
+
+tasks.bootJar {
+    enabled = false
 }
 
 dependencies {
--- spring-boot/build.gradle.kts
+++ spring-boot/build.gradle.kts
@@ -1,9 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    kotlin("jvm")
     id("org.springframework.boot") version "2.2.6.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
-    kotlin("jvm")
     kotlin("plugin.spring") version "1.3.71"
 }
 
