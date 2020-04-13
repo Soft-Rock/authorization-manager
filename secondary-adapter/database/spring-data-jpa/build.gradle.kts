9113dc60555da2981e7c9108aac581e12907a30c
David Gracia Celemendi <david.gracia.celemendi@gmail.com>
2020-04-13 22:17:48 +0200

    change

--- secondary-adapter/database/spring-data-jpa/build.gradle.kts
+++ secondary-adapter/database/spring-data-jpa/build.gradle.kts
@@ -11,7 +11,6 @@ tasks.bootJar {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
-//    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation(project(":interaction"))
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 }
