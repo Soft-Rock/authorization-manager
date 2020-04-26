2a509165712971c56990c578b17d357f080ae5ce
David Gracia Celemendi <david.gracia.celemendi@gmail.com>
2020-04-26 12:38:45 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 // PLUGINS -- BEGIN
 plugins {
     kotlin("jvm") version "1.3.72"
-    `java-library`
     jacoco
     id("org.sonarqube") version "2.8"
     id("com.jfrog.bintray") version "1.8.5"
