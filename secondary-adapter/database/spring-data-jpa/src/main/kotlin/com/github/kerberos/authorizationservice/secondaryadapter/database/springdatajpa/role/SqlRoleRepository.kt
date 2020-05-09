41c67690069db18a4dc3d681490fbbd9bd010cfe
Cristina Verdi <cristina.verdi.tobon@gmail.com>
2020-05-09 20:40:44 +0200

    Add 'smart' save role test unhappy path

--- secondary-adapter/database/spring-data-jpa/src/main/kotlin/com/github/kerberos/authorizationservice/secondaryadapter/database/springdatajpa/role/SqlRoleRepository.kt
+++ secondary-adapter/database/spring-data-jpa/src/main/kotlin/com/github/kerberos/authorizationservice/secondaryadapter/database/springdatajpa/role/SqlRoleRepository.kt
@@ -4,19 +4,24 @@ import com.github.kerberos.authorizationservice.domain.role.Role
 import com.github.kerberos.authorizationservice.domain.role.RoleId
 import com.github.kerberos.authorizationservice.interaction.role.RoleRepository
 import javax.inject.Named
+import javax.persistence.EntityExistsException
+import javax.persistence.EntityNotFoundException
+import org.springframework.data.repository.findByIdOrNull
 
 @Named
 class SqlRoleRepository(
     private val jpaRoleRepository: JpaRoleRepository
 ) : RoleRepository {
     override fun save(role: Role) {
-        jpaRoleRepository
-        .save(role.toJpaRole())
+        if (!jpaRoleRepository.existsById(role.id.toString())) {
+            jpaRoleRepository.save(role.toJpaRole())
+        } else {
+            throw EntityExistsException()
+        }
     }
 
     override fun findById(roleId: RoleId): Role =
             jpaRoleRepository
-                    .findById(roleId.value.toString())
-                    .orElse(null)
-                    .toRole()
+                    .findByIdOrNull(roleId.value.toString())
+                    ?.toRole() ?: throw EntityNotFoundException()
 }
--- secondary-adapter/database/spring-data-jpa/src/test/kotlin/com/github/kerberos/authorizationservice/secondaryadapter/database/springdatajpa/role/SqlRoleRepositoryTest.kt
+++ secondary-adapter/database/spring-data-jpa/src/test/kotlin/com/github/kerberos/authorizationservice/secondaryadapter/database/springdatajpa/role/SqlRoleRepositoryTest.kt
@@ -5,23 +5,36 @@ import com.github.kerberos.authorizationservice.domain.role.RoleDescription
 import com.github.kerberos.authorizationservice.domain.role.RoleId
 import com.github.kerberos.authorizationservice.domain.role.RoleName
 import java.util.UUID
+import javax.persistence.EntityExistsException
 import org.amshove.kluent.`should be equal to`
+import org.amshove.kluent.`should throw`
+import org.amshove.kluent.invoking
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 
 @DataJpaTest
-internal class SqlRoleRepositoryTest @Autowired constructor(jpaRoleRepository: JpaRoleRepository) {
-
+internal class SqlRoleRepositoryTest @Autowired constructor(
+    jpaRoleRepository: JpaRoleRepository
+) {
     private val sqlRoleRepository: SqlRoleRepository = SqlRoleRepository(jpaRoleRepository)
 
     @Test
-    fun `save role`() {
+    fun `don't save a role if it already exists`() {
+        `save role`()
+        `attempt to save role again`() `should throw` EntityExistsException::class
+    }
+
+    @Test
+    fun `successfully save a role`() {
+        `save role`()
+        `retrieved role`() `should be equal to` expectedRole
+    }
+
+    private fun `retrieved role`() = sqlRoleRepository.findById(roleId)
+    private fun `attempt to save role again`() = invoking { `save role`() }
+    private fun `save role`() {
         sqlRoleRepository.save(expectedRole)
-
-        val role = sqlRoleRepository.findById(roleId)
-
-        role `should be equal to` expectedRole
     }
 
     companion object {
