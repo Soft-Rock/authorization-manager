e1c9a1cf55764e1b5cbe2ebd663df58fc322aa2b
Cristina Verdi <cristina.verdi.tobon@gmail.com>
2020-05-09 21:03:55 +0200

    Update CreateRoleBody

--- primary-adapter/rest/spring-webmvc/src/main/kotlin/com/github/kerberos/authorizationservice/primaryadapter/rest/springwebmvc/role/create/CreateRoleBody.kt
+++ primary-adapter/rest/spring-webmvc/src/main/kotlin/com/github/kerberos/authorizationservice/primaryadapter/rest/springwebmvc/role/create/CreateRoleBody.kt
@@ -1,7 +1,6 @@
 package com.github.kerberos.authorizationservice.primaryadapter.rest.springwebmvc.role.create
 
 data class CreateRoleBody(
-    val id: String,
     val name: String,
     val description: String
 )
--- primary-adapter/rest/spring-webmvc/src/main/kotlin/com/github/kerberos/authorizationservice/primaryadapter/rest/springwebmvc/role/create/CreateRoleController.kt
+++ primary-adapter/rest/spring-webmvc/src/main/kotlin/com/github/kerberos/authorizationservice/primaryadapter/rest/springwebmvc/role/create/CreateRoleController.kt
@@ -2,8 +2,10 @@ package com.github.kerberos.authorizationservice.primaryadapter.rest.springwebmv
 
 import com.github.kerberos.authorizationservice.interaction.cqrs.CommandBus
 import com.github.kerberos.authorizationservice.interaction.role.create.CreateRoleCommand
+import org.springframework.http.HttpStatus.CREATED
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -11,6 +13,7 @@ class CreateRoleController(
     private val commandBus: CommandBus
 ) {
     @PostMapping("/roles")
+    @ResponseStatus(CREATED)
     fun createRole(@RequestBody body: CreateRoleBody) {
         commandBus.dispatch(
             CreateRoleCommand(
