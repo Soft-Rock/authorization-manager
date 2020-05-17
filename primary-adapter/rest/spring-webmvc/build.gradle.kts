874a51abfb47ae2d037512a2c47e6f4ade074ad7
David Gracia Celemendi <david.gracia.celemendi@gmail.com>
2020-05-17 13:39:48 +0200

    fix gradle configuration

--- primary-adapter/rest/spring-webmvc/build.gradle.kts
+++ primary-adapter/rest/spring-webmvc/build.gradle.kts
@@ -1,19 +1,14 @@
 plugins {
     kotlin("jvm")
     `java-library`
-    id("org.springframework.boot") version "2.2.6.RELEASE"
+    id("org.springframework.boot") version "2.2.6.RELEASE" apply (false)
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
     kotlin("plugin.spring") version "1.3.72"
-}
-
-tasks.bootJar {
-    enabled = false
 }
 
 dependencies {
     implementation(project(":interaction"))
 
-    // implementation("org.hibernate:hibernate-validator:4.2.0.Final")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.boot:spring-boot-starter-web:2.2.6.RELEASE")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.11.0")
 }
